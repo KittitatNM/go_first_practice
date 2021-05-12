6305dac02e2fa4e7e3989dbc49b5b5d9701d2505
unknown <kittitat.nm@gmail.com>
2021-05-12 16:10:43 +0700

    04-function

---
+++ 04-function/func-value.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+var plus func(a, b int) int = func(a, b int) int { return a + b }
+
+func cal(op func(int, int) int) {
+	result := op(4, 5)
+	fmt.Println("Cal result is : ", result)
+}
+
+func main() {
+	p := plus(1, 2)
+	fmt.Println(p)
+	fmt.Printf("Type of %T\n", plus)
+	cal(plus)
+
+	minus := func(a, b int) int { return a - b }
+	cal(minus)
+}
---
+++ 04-function/func.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func info() {
+	name := "Gopher"
+	fmt.Println("Hello ", name)
+}
+func info1(name, msg string, age int) {
+	fmt.Println("Hello ", name)
+	fmt.Println("Hello ", msg)
+	fmt.Println("Hello ", age)
+}
+
+func today() string {
+	return time.Saturday.String()
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func main() {
+	info()
+	info1("Mild", "test", 10)
+	fmt.Println(today())
+
+	x, y := swap("Left", "Right")
+
+	fmt.Printf("Left is %v : Right is %v", x, y)
+}
