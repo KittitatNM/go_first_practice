091ed4c9d8d67886f731d64cd82e374256878a67
unknown <kittitat.nm@gmail.com>
2021-05-12 15:07:58 +0700

    01-main

--- 01-main/main.go
+++ 01-main/main.go
@@ -4,4 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("hello Gopher")
+	fmt.Printf("Hello %s", "Gopher")
+	fmt.Println("next line")
+
+	fmt.Printf("IS int %d\n", 50)
+	fmt.Printf("IS int %v\n", "50")
 }
